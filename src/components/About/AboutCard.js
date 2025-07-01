d78d97082101e66eff2cece949386c2fa232c325
Mohamed Washif <washif@digiledge.in>
2025-07-01 16:32:45 +0530

    unused fix

--- src/components/About/AboutCard.js
+++ src/components/About/AboutCard.js
@@ -1,6 +1,6 @@
 import React from "react";
 import Card from "react-bootstrap/Card";
-import { ImCircleRight, ImPointRight } from "react-icons/im";
+import { ImCircleRight } from "react-icons/im";
 
 function AboutCard() {
   return (
--- src/components/About/Toolstack.js
+++ src/components/About/Toolstack.js
@@ -3,7 +3,6 @@ import { Col, Row } from "react-bootstrap";
 import {
   SiVisualstudiocode,
   SiDocker,
-  SiWindows,
   SiUbuntu,
   SiGit,
   SiGithub,
